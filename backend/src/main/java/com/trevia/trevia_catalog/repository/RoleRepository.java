2396ad41ab092084fdc887e7853b9ec34fd1aeca
kaveeshakalanka <kaveeshakalanka0129@gmail.com>
2026-02-12 13:27:42 +0530

    Update files backend-loginrequest/orderrequest/signuprequest/Role/RoleRepository

--- backend/src/main/java/com/trevia/trevia_catalog/repository/RoleRepository.java
+++ backend/src/main/java/com/trevia/trevia_catalog/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.trevia.trevia_catalog.repository;
 
-public class RoleRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.trevia.trevia_catalog.entity.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(Role.ERole name);
 }
